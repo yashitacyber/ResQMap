4a79d7bc459236a715c412cc6e645f6037161795
Babul Kumar <BabulKumar100@users.noreply.github.com>
2026-03-03 11:58:35 +0530

    Update locationRoutes file

--- server/src/routes/locationRoutes.js
+++ server/src/routes/locationRoutes.js
@@ -1,13 +1,6 @@
 const router = require('express').Router();
 const locationController = require('../controllers/locationController');
 
-// Routes for locations
-router.get('/', (req, res) => {
-    res.json({ message: 'Locations endpoint' });
-});
-
-router.get('/nearby', (req, res) => {
-    res.json({ message: 'Nearby locations endpoint' });
-});
+router.get('/', locationController.getLocations);
 
 module.exports = router;
