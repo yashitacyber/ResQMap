f2bb370ce3d7a2ab589653961c9684400d7ab7d1
Babul Kumar <199328506+BabulKumar100@users.noreply.github.com>
2026-03-23 23:55:12 +0530

    Fix #38: secured env files, added .env.example, and fixed invalid file issue

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../config/db");
+
 
 // return dashboard statistics
 router.get("/analytics/metrics", async (req, res) => {
